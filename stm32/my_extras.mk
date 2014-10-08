f4f186a6803b78f7b36ee940b12b2581f52fbbb2
Ethan Durrant <emd519@yahoo.com>
2014-10-08 13:37:18 -0700

    makefile additions

---
+++ stm32/my_extras.mk
@@ -0,0 +1,24 @@
+
+Q := @
+STM32LOADER_PY  ?= stm32loader.py
+STM32LOADER_ARGS ?= -e -w -v 
+STM32LOADER_PORT ?= /dev/ttyUSB0
+STFLASH         = $(shell which st-flash)
+
+
+#my extra stuff to add to the makefiles
+
+debug_gdb:
+	arm-none-eabi-gdb -ex 'target extended-remote :4242' $(BUILDDIR)/$(PROJECT).elf
+
+serial_bootloader_flash:
+	$(STM32LOADER_PY) $(STM32LOADER_ARGS) -p $(STM32LOADER_PORT) $(BUILDDIR)/$(PROJECT).bin
+
+#%.stlink-flash: %.bin
+#        @printf "  FLASH  $<\n"
+#        $(Q)$(STFLASH) write $(*).bin 0x8000000
+stlink-flash:
+	@printf "  FLASH  $<\n"
+	$(Q)$(STFLASH) write $(*).bin 0x8000000
+
+
