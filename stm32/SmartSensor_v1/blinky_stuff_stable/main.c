007f9e985e20d9f08aee83724e451ec644618789
Ethan Durrant <emd519@yahoo.com>
2014-10-08 18:34:11 -0700

    stuff is working, turned out to be a hardware issue

--- stm32/SmartSensor_v1/blinky_stuff_stable/main.c
+++ stm32/SmartSensor_v1/blinky_stuff_stable/main.c
@@ -1,12 +1,13 @@
 #include "ch.h"
 #include "hal.h"
-#include "test.h"
+//#include "test.h"
 
-//#define CH_NO_IDLE_THREAD
+//#define CH_NO_IDLE_THREAD TRUE
 
 /*
  * Blinker thread.
  */
+#if 0
 static THD_WORKING_AREA(waThread1, 128);
 static THD_FUNCTION(Thread1, arg) {
 
@@ -20,22 +21,27 @@ static THD_FUNCTION(Thread1, arg) {
     chThdSleepMilliseconds(500);
   }
 }
-
+#endif
 
 int main(void){
     
     halInit();
     chSysInit();
     
-    chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO+1, Thread1, NULL);
+    //chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO+1, Thread1, NULL);
 
 
     palSetPadMode(GPIOA, 3, PAL_MODE_OUTPUT_PUSHPULL);
 
-    while (true){
+    while (TRUE){
         palSetPad(GPIOA, 3);
         chThdSleepMilliseconds(500);
+        //volatile unsigned int i = 1000000;
+        //while(i--);
         palClearPad(GPIOA, 3);
+        //volatile unsigned int 
+        //i = 1000000;
+        //while(i--);
         chThdSleepMilliseconds(500);
     }
 }
--- stm32/my_boards/SMARTSENSOR_V1_STM32_F103C8/board.mk
+++ stm32/my_boards/SMARTSENSOR_V1_STM32_F103C8/board.mk
@@ -1,5 +1,7 @@
 # List of all the board related files.
-BOARDSRC = ${CHIBIOS}/os/hal/boards/SMARTSENSOR_V1_STM32_F103C8/board.c
+#BOARDSRC = ${CHIBIOS}/os/hal/boards/SMARTSENSOR_V1_STM32_F103C8/board.c
+BOARDSRC = ${MY_STM32_PATH}/my_boards/SMARTSENSOR_V1_STM32_F103C8/board.c
 
 # Required include directories
-BOARDINC = ${CHIBIOS}/os/hal/boards/SMARTSENSOR_V1_STM32_F103C8
+#BOARDINC = ${CHIBIOS}/os/hal/boards/SMARTSENSOR_V1_STM32_F103C8
+BOARDINC = ${MY_STM32_PATH}/my_boards/SMARTSENSOR_V1_STM32_F103C8
