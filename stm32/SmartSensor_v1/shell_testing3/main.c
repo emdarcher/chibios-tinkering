05463cf0b6fcc46ec95479f1be4159ba565eb2ad
Ethan Durrant <emd519@yahoo.com>
2014-12-28 23:12:04 -0800

    stuff is finally working, added SHELL_MAX_ARGUMENTS override in Makefile stuff

--- stm32/SmartSensor_v1/shell_testing3/main.c
+++ stm32/SmartSensor_v1/shell_testing3/main.c
@@ -21,7 +21,8 @@
 #define MY_ABS(a) (((a)>=0) ? (a) : (0L-(a)))  
 
 #define SHELL_WA_SIZE THD_WA_SIZE(1024)
-
+/* SHELL_MAX_ARGUMENTS can be changed in the Makefile */
+//#define     SHELL_MAX_ARGUMENTS   6
 
 /* config for the serial stuff */
 static SerialConfig sd1conf = {
@@ -98,9 +99,31 @@ static __attribute__((noreturn)) msg_t SerOutThr1(void *arg){
 }
 #endif
 
+#define CMD_STR_ACCEL "accel"
+#define CMD_STR_COLOR "color"
+#define CMD_STR_LED "led"
+#define CMD_STR_LOOP "loop"
+
+enum{CMD_ACCEL,CMD_COLOR,CMD_LED,CMD_LOOP,  };
+const char * cmd_name_strings[] = {
+CMD_STR_ACCEL,CMD_STR_COLOR,CMD_STR_LED,CMD_STR_LOOP,
+};
+/* stores number of strings in the cmd_name_strings array */
+const uint8_t cmd_name_strings_len = 
+    sizeof(cmd_name_strings) / sizeof(cmd_name_strings[0]);
+
+static void call_cmd_from_index( BaseSequentialStream *chp, int argc, char *argv[], uint8_t cmd_index);
+
 static const char invalid_num_str[] = "%s is not a valid number!\n\r";
 static const char invalid_arg_str[] = "%s is an invalid argument!\n\r";
 
+static const char color_usage[] = "usage: color [ -h | -l <loop_times>]\n\r"
+                    "\t-h: prints help\n\r"
+                    "\t-l <loop_times> : loops the amount of times\n\r";
+
+static const char loop_usage[] = "loop usage: loop <command> [ -h | -l <loop_times>]\n\r"
+                    "\t-h: prints help\n\r"
+                    "\t-l <loop_times> : loops the amount of times\n\r";
 static uint8_t str_is_valid_num(char * str){
     uint8_t nI;
     for(nI=0;nI<strlen(str);nI++){
@@ -126,27 +149,13 @@ static void cmd_accel(BaseSequentialStream *chp, int argc, char *argv[]) {
         accel_x,accel_y,accel_z);
 }
 static void cmd_color(BaseSequentialStream *chp, int argc, char *argv[]) {
+    #define COLOR_USABLE_WITH_LOOP TRUE
+    #if !COLOR_USABLE_WITH_LOOP 
     uint32_t loop_times=1;
     uint32_t loop_delay_ms=100;
-    const char usage[] = "usage: color [ -h | -l <loop_times>]\n\r"
-                    "\t-h: prints help\n\r"
-                    "\t-l <loop_times> : loops the amount of times\n\r";
     if(argc > 0){
         if(strcmp("-l", argv[0])==0){
             if(argc > 1){
-                #if 0
-                uint8_t nI;
-                for(nI=0;nI<strlen(argv[1]);nI++){
-                    char chk = argv[1][nI];
-                    /* checks for any non-numerical characters in the argument */
-                    if(!((chk>=48)&&(chk<=57))){
-                        chprintf(chp,"%s is not a valid number!\n\r", argv[1]);
-                        cmd_error=1;
-                        break;
-                    }
-                }
-                #endif
-                
                 if(str_is_valid_num(argv[1])){ 
                     loop_times = atol(argv[1]);
                     if(argc > 2){
@@ -172,15 +181,16 @@ static void cmd_color(BaseSequentialStream *chp, int argc, char *argv[]) {
             }
         } else if(strcmp("-h",argv[0])==0){
             chprintf(chp, "printing help:\n\r");
-            chprintf(chp, usage);
+            chprintf(chp, color_usage);
         } else {
             chprintf(chp, invalid_arg_str, argv[0]);
-            chprintf(chp, usage);
+            chprintf(chp, color_usage);
         }
     }
     chprintf(chp, "looping %U times with delay of %U milliseconds:\n\r", 
         loop_times,loop_delay_ms);
     while(loop_times--){
+    #endif
         #if !USE_I2C_POLL_THD 
         request_color_data();
         #endif
@@ -189,8 +199,10 @@ static void cmd_color(BaseSequentialStream *chp, int argc, char *argv[]) {
         chSysUnlockFromIsr();
         chprintf(chp, "color_lx: y=%U\tr=%U\tg=%U\tb=%U\n\r",
             y_lx_val,r_lx_val,g_lx_val,b_lx_val);
+    #if !COLOR_USABLE_WITH_LOOP 
         chThdSleepMilliseconds(loop_delay_ms);
     }
+    #endif
 }
 
 static void cmd_led(BaseSequentialStream *chp, int argc, char *argv[]) {
@@ -213,11 +225,100 @@ static void cmd_led(BaseSequentialStream *chp, int argc, char *argv[]) {
         chprintf(chp, "usage: led <led_state>\n\r\tled_state: 0=OFF 1=ON t=TOGGLE\n\r");
     }
 }
+static void cmd_loop(BaseSequentialStream *chp, int argc, char *argv[]) {
+    uint32_t loop_times=1;
+    uint32_t loop_delay_ms=100;
+    uint8_t cmd_match=0;
+    int pass_argc=0;
+    char *pass_argv[0];
+if(argc > 0){
+    uint8_t cmd_i;
+    for(cmd_i=0;cmd_i<cmd_name_strings_len;cmd_i++){
+        if(strcmp(cmd_name_strings[cmd_i],argv[0])==0){
+            cmd_match++;
+            break;
+        }
+    }
+    if(cmd_match==1){
+    if(argc > 1){
+        if(strcmp("-l", argv[1])==0){
+            if(argc > 2){
+                if(str_is_valid_num(argv[2])){ 
+                    loop_times = atol(argv[2]);
+                    if(argc > 3){
+                        if(strcmp("-s", argv[3])==0){
+                            if(argc > 4){
+                                if(str_is_valid_num(argv[4])){
+                                    loop_delay_ms = atol(argv[4]);
+                                } else {
+                                    chprintf(chp,invalid_num_str,argv[4]);
+                                }
+                            } else {
+                                chprintf(chp,"please enter millisecond delay\n\r");
+                            }
+                        } else {
+                            chprintf(chp,invalid_arg_str,argv[3]);
+                        }
+                    }  
+                } else {
+                    chprintf(chp,invalid_num_str, argv[2]);
+                }
+            } else {
+                chprintf(chp, "please enter a number of times to loop\n\r");
+            }
+        } else if(strcmp("-h",argv[1])==0){
+            //chprintf(chp, "printing help:\n\r");
+            chprintf(chp, loop_usage);
+        } else {
+            chprintf(chp, invalid_arg_str, argv[1]);
+            chprintf(chp, loop_usage);
+        }
+    }
+    chprintf(chp, "looping %U times with delay of %U milliseconds:\n\r", 
+        loop_times,loop_delay_ms);
+
+    while(loop_times--){
+        call_cmd_from_index(chp,pass_argc,pass_argv,cmd_i);        
+        chThdSleepMilliseconds(loop_delay_ms);
+    } 
+
+    } else if(strcmp("-h",argv[0])==0){
+        chprintf(chp, loop_usage);
+    } else {
+        chprintf(chp, "%s is not a designated command!\n\r", argv[0]);
+        chprintf(chp, loop_usage);
+    }
+
+
+} else {
+    chprintf(chp, "please enter a cmd to loop\n\r");
+    chprintf(chp, loop_usage);
+}
+
+}
+
+static void call_cmd_from_index( BaseSequentialStream *chp, int argc, char *argv[], uint8_t cmd_index){
+    switch (cmd_index){
+    case CMD_ACCEL:
+        cmd_accel(chp, argc, argv);
+        break;
+    case CMD_COLOR:
+        cmd_color(chp, argc, argv);
+        break;
+    case CMD_LED:
+        cmd_led(chp, argc, argv);
+        break;
+    case CMD_LOOP:
+        cmd_loop(chp, argc, argv);
+        break;
+    }
+}
 
 static const ShellCommand shCmds[] = {
-    {"accel", cmd_accel},
-    {"color", cmd_color},
-    {"led", cmd_led},
+    {CMD_STR_ACCEL, cmd_accel},
+    {CMD_STR_COLOR, cmd_color},
+    {CMD_STR_LED,cmd_led},   
+    {CMD_STR_LOOP,cmd_loop},
     {NULL, NULL}
 };
 static const ShellConfig shCfg = {
